92428957f585d114b3f9317f3722658ec2fff971
201242274 <Tao.Zhang@hytera.com>
2022-07-05 09:59:35 +0800

    第一次提交

---
+++ src/main/java/com/zt/entity/UserEntity.java
@@ -0,0 +1,11 @@
+package com.zt.entity;
+
+/**
+ * @Author: zt  2022/7/5 9:57
+ * @CreateTime: 2022/7/5 9:57
+ * @描述：
+ **/
+public class UserEntity {
+    private String username;
+    private String password;
+}
